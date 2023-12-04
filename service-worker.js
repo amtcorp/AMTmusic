c704fc73510bab57c6ca979e83a39affe17c0a33
amtcorp <141008517+amtcorp@users.noreply.github.com>
2023-12-04 04:56:48 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+// service-worker.js
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('app-cache').then(function(cache) {
+      return cache.addAll([
+        '/', // Mettez ici les URL des ressources à mettre en cache
+        './index.html',
+        './style.css',
+        '/scripts.js'
+        // Ajoutez ici d'autres ressources statiques à mettre en cache
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
